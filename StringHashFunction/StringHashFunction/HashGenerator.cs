65c5484ff016869812b4b64cd9deb12df23e3c80
Ivan Danilyuk <werwetsad@gmail.com>
2023-01-30 17:22:24 +0500

    Что-то там считается теперь, вопрос в эффективности

---
+++ StringHashFunction/StringHashFunction/HashGenerator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringHashFunction
+{
+    public class HashGenerator : IGeneratable
+    {
+        public string? Password { get; set; }
+        public string GenerateHash(string Key)
+        {
+            string result = "";
+            string temp = CalculateDirection().ToString();
+            for (int i = 1; i < Key.Length + 1; i++)
+            {
+                int d = i % temp.Length;
+                char t = (char)((Key[i - 1] << (d % 128)) % 125);
+                if (t > 32) result += (char)t;
+                else result += (char)(t + 33);
+                if (result.Length >= Password.Length << 2) break;
+            }
+            return result;
+        }
+        
+        private string CalculateDirection()
+        {
+            string res = "";
+            if (this.Password != null) foreach (var e in Password) res += (char)(e << 1);
+            return res;
+        }
+    }
+}
--- StringHashFunction/StringHashFunction/Program.cs
+++ StringHashFunction/StringHashFunction/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace StringHashFunction
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            HashGenerator h = new HashGenerator() { Password  = "1ываdcr343"};
+            Console.WriteLine(h.GenerateHash("C5"));
+            Console.WriteLine(h.GenerateHash("zk.,k.fkbye"));
+            Console.WriteLine(h.GenerateHash("cnfcvjkjltw"));
+            Console.WriteLine(h.GenerateHash("dctvGhbDTTTTN!!!D"));
+            Console.WriteLine(h.GenerateHash("C524uhyc67qwhu jqtp,wr9m-f0iej4h204um "));
+        }
+    }
+}
